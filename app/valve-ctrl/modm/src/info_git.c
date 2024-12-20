52670ff84051b3fa7df9cc38c7236a2029cef0ff
tecnologic <tecnologic86@gmail.com>
2024-12-20 22:45:23 +0100

    extracted the bsb

---
+++ app/valve-ctrl/modm/src/info_git.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019, 2021, Niklas Hauser
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include "info_git.h"
+
+
+const int16_t MODM_GIT_ADDED = 0;
+const char *MODM_GIT_AUTHOR = "tecnologic";
+const char *MODM_GIT_AUTHOR_DATE = "2024-12-20 22:28:30+01:00";
+const uint32_t MODM_GIT_AUTHOR_DATE_TIMESTAMP = 1734730110;
+const char *MODM_GIT_AUTHOR_EMAIL = "tecnologic86@gmail.com";
+const char *MODM_GIT_COMMITTER = "tecnologic";
+const char *MODM_GIT_COMMITTER_DATE = "2024-12-20 22:28:30+01:00";
+const uint32_t MODM_GIT_COMMITTER_DATE_TIMESTAMP = 1734730110;
+const char *MODM_GIT_COMMITTER_EMAIL = "tecnologic86@gmail.com";
+const char *MODM_GIT_CONFIG_USER_EMAIL = "tecnologic86@gmail.com";
+const char *MODM_GIT_CONFIG_USER_NAME = "tecnologic";
+const int16_t MODM_GIT_COPIED = 0;
+const int16_t MODM_GIT_DELETED = 0;
+const int16_t MODM_GIT_MODIFIED = 2;
+const int16_t MODM_GIT_RENAMED = 0;
+const char *MODM_GIT_SHA = "8e2f3cc6bdc7a48a2fa7816ebcf74f3ca4dd7b6b";
+const char *MODM_GIT_SHA_ABBR = "8e2f3cc6bd";
+const char *MODM_GIT_SUBJECT = "generated modm vscode files";
+const int16_t MODM_GIT_UNTRACKED = 35;
