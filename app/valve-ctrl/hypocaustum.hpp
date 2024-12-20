5d33b147451a471a0c504ff7191bca0667cdc07f
tecnologic <tecnologic86@gmail.com>
2024-12-20 21:40:35 +0100

    started with the licensing stuff. GPL is not compatible with modm so we change to modm

---
+++ app/valve-ctrl/hypocaustum.hpp
@@ -0,0 +1,39 @@
+/*
+//////////////////////////////////////////////////////////////////////
+// _   _                                       _                    //
+//| | | |_   _ _ __   ___   ___ __ _ _   _ ___| |_ _   _ _ __ ___   //
+//| |_| | | | | '_ \ / _ \ / __/ _` | | | / __| __| | | | '_ ` _ \  //
+//|  _  | |_| | |_) | (_) | (_| (_| | |_| \__ \ |_| |_| | | | | | | //
+//|_| |_|\__, | .__/ \___/ \___\__,_|\__,_|___/\__|\__,_|_| |_| |_| //
+//       |___/|_|                                                   //
+//////////////////////////////////////////////////////////////////////
+
+A under floor heating control system for DC-Mootor Vales like HmIP VDMOT
+This project is based on the modm library.
+
+Copyright(C) 2024 Alexander Evers
+
+This program is free software;
+you can redistribute it and / or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation;
+either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program;
+if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110 - 1301, USA
+*/
+#ifndef HYPOCAUSTUM_HPP
+#define HYPOCAUSTUM_HPP
+
+
+
+
+
+
+#endif // HYPOCAUSTUM_HPP
--- app/valve-ctrl/main.cpp
+++ app/valve-ctrl/main.cpp
@@ -1,14 +1,33 @@
-/*
- * Copyright (c) 2013, Fabian Greif
- * Copyright (c) 2013, Kevin Läufer
- * Copyright (c) 2014-2018, Niklas Hauser
- *
- * This file is part of the modm project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+/* 
+////////////////////////////////////////////////////////////////////// 
+// _   _                                       _                    //
+//| | | |_   _ _ __   ___   ___ __ _ _   _ ___| |_ _   _ _ __ ___   //
+//| |_| | | | | '_ \ / _ \ / __/ _` | | | / __| __| | | | '_ ` _ \  //
+//|  _  | |_| | |_) | (_) | (_| (_| | |_| \__ \ |_| |_| | | | | | | //
+//|_| |_|\__, | .__/ \___/ \___\__,_|\__,_|___/\__|\__,_|_| |_| |_| //
+//       |___/|_|                                                   //
+//////////////////////////////////////////////////////////////////////
+
+A under floor heating control system for DC-Mootor Vales like HmIP VDMOT
+This project is based on the modm library.
+
+Copyright(C) 2024 Alexander Evers
+
+This program is free software;
+you can redistribute it and / or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation;
+either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program;
+if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110 - 1301, USA
+*/
 // ----------------------------------------------------------------------------
 #include <modm/board.hpp>
 #include <modm/debug.hpp>
@@ -21,7 +40,7 @@
 #undef MODM_LOG_LEVEL
 #define MODM_LOG_LEVEL modm::log::INFO
 
-Rtt rtt(0);
+																												 Rtt rtt(0);
 modm::IODeviceObjectWrapper<Rtt, modm::IOBuffer::DiscardIfFull> rtt_device(rtt);
 // Set all four logger streams to use RTT
 modm::log::Logger modm::log::debug(rtt_device);
